7d9321c1f6300cb16992a0c505d9eb4842776178
ShiDongjie <dongjie.shi@baidao.com>
2017-02-03 18:26:48 +0800

    save

--- ios/ytxinner/AppDelegate.m
+++ ios/ytxinner/AppDelegate.m
@@ -8,6 +8,7 @@
  */
 
 #import "AppDelegate.h"
+#import "CodePush.h"
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
--- src/containers/liveroom/RoomList.js
+++ src/containers/liveroom/RoomList.js
@@ -1,6 +1,6 @@
 'use strict';
 import React, { Component, PropTypes } from 'react';
-import { ScrollView, Image, View,RefreshControl ,Text} from 'react-native';
+import { ScrollView, Image, View,RefreshControl ,Text,StyleSheet} from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 import isEmpty from 'lodash/isEmpty';
@@ -10,19 +10,26 @@ class RoomList extends Component {
   constructor() {
     super();
   }
-  
+
   render() {
     const { userReducer,favorites } = this.props;
 
     return (
-      <Image style={{flex:1,width: null,height: null,paddingTop:64,backgroundColor:'white'}}>
-        <ScrollView
-          automaticallyAdjustContentInsets={false}
-          contentInset={{bottom:40}}
-        >
-          <Text>roomlist</Text>
-        </ScrollView>
-      </Image>
+      <View style={styles.parent}>
+
+        <Text style={styles.aaa}>
+        11111
+        </Text>
+
+        <Text style={styles.bbb} >
+        222
+        </Text>
+
+        <Text style={styles.ccc} >
+        333
+        </Text>
+
+      </View>
     );
   }
 }
@@ -34,5 +41,41 @@ function mapStateToProps(state) {
 
   }
 }
+
+var styles = StyleSheet.create({
+  parent: {
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#ddc',
+  },
+
+  aaa:{
+    width:100,
+    height:100,
+    color:'green',
+    textAlign: 'center',
+    fontSize: 20,
+    backgroundColor: '#a9ea00',
+    lineHeight: 60,
+  },
+  bbb:{
+    width:100,
+    height:100,
+    color:'#fff',
+    textAlign: 'center',
+    backgroundColor: 'black',
+    lineHeight: 100,
+  },
+
+  ccc:{
+    width:100,
+    height:100,
+    color:'blue',
+    textAlign: 'center',
+    backgroundColor: 'pink',
+  },
+});
 
 export default connect(mapStateToProps)(RoomList);
